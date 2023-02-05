6a43349b5acd2245e5d0b69227b6b9b10edabf42
aarontravass <aaron.travass@hotmail.com>
2023-02-05 06:28:48 -0500

    fixed bugs

--- src/kroger/python_kroger_client/config.py
+++ src/kroger/python_kroger_client/config.py
@@ -1,10 +1,12 @@
 # Import all the necessary packages
 import base64
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 # Get these 3 values from registering your developer account/application with https://developer.kroger.com/
-client_id     = 'hackviolet23-d5777aed77e91b1d981a426eb57347d83948257829297173864'
-client_secret = 'T53Tk6KemXTMQ5DniMFZhhWHI6soS0wPkO0LHeMu'
-redirect_uri  = 'http://localhost:5000'
+client_id     = os.getenv('KROGER_CLIENT_ID')
+client_secret = os.getenv('KROGER_CLIENT_SECRET')
+redirect_uri  = os.getenv('KROGER_REDIRECT_URI')
 
 # Authentication requires base64 encoded id:secret, which is precalculated here
 encoded_client_token = base64.b64encode(f"{client_id}:{client_secret}".encode('ascii')).decode('ascii')
