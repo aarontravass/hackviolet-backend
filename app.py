a03af476e5eee7049c9dad4a5a1920bbf9970c9d
aarontravass <aaron.travass@hotmail.com>
2023-02-04 23:00:19 -0500

    added auth

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ from flask import Flask, Response, request
 import os
 from dotenv import load_dotenv
 import src.auth.auth as auth
+import json
 load_dotenv()
 
 app = Flask(__name__)
@@ -11,10 +12,20 @@ app = Flask(__name__)
 @app.route("/login", methods = ['POST'])
 def login():
     print(request.json)
-    response = auth.login(request.json)
-    return Response(response, status=response.get('statusCode'), mimetype='application/json')
+    res = auth.login(request.json)
+    return Response(response=json.dumps(res), status=res.get('statusCode'), mimetype='application/json')
 
+@app.route("/register", methods = ['POST'])
+def register():
+    print(request.json)
+    res = auth.register(request.json)
+    return Response(response=json.dumps(res), status=res.get('statusCode'), mimetype='application/json')
 
+@app.route("/logout", methods = ['POST'])
+def logout():
+    authHeader = request.headers.get('Authorization')
+    res = auth.logout(request.json, authHeader)
+    return Response(response=json.dumps(res), status=res.get('statusCode'), mimetype='application/json')
 
 
 if __name__ == "__main__":
--- src/auth/auth.py
+++ src/auth/auth.py
@@ -1,9 +1,11 @@
 import src.database.mongodb.config as mongo
 import bcrypt
 import jwt
+import hashlib
+import uuid
+import json
 
 instanceDB = mongo.getDb().get_database('public')
-
 
 users_collection = instanceDB['users']
 authTable = instanceDB['auth_token']
@@ -15,21 +17,30 @@ authTable = instanceDB['auth_token']
 
 
 def login(body: dict) -> dict:
-    response = {}
     email = body.get('email')
     password = body.get('password')
     if email is None or password is None:
         response = {
             'statusCode': 400,
+            'status': False,
             'message': 'Email and Password is required'
         }
         return response
-    hashed = bcrypt.hashpw(str.encode(password), bcrypt.gensalt()).decode('utf-8')
-    mongo_res = users_collection.find_one({'email': email, 'password': hashed})
+    hashed_string = hashlib.sha256(password.encode('utf-8')).hexdigest()
+
+    mongo_res = users_collection.find_one({'email': email})
     if mongo_res is None:
         response = {
             'statusCode': 401,
-            'message': 'Invalid User Credentials'
+            'message': 'Invalid User Credentials',
+            'status': False
+        }
+        return response
+    if not bcrypt.checkpw(hashed_string.encode('utf-8'), mongo_res.get('password').encode('utf-8')):
+        response = {
+            'statusCode': 401,
+            'message': 'Invalid Password',
+            'status': False
         }
         return response
     payload = {
@@ -40,16 +51,96 @@ def login(body: dict) -> dict:
         'auth_token': auth_token,
         'user_guid': mongo_res['user_guid']
     }
+
     authTable.insert_one(insert_body);
     response = {
         'statusCode': 200,
-        'data': insert_body
+        'message': None,
+        'status': True,
+        'data': {
+            'auth_token': auth_token,
+            'user_guid': mongo_res['user_guid']
+        }
+    }
+    print(response)
+
+    return response
+
+
+def register(body: dict) -> dict:
+    first_name = body.get('first_name')
+    last_name = body.get('last_name')
+    email = body.get('email')
+    password = body.get('password')
+    if not first_name and not last_name and not email and not password:
+        response = {
+            'statusCode': 400,
+            'status': False,
+            'message': 'First Name, Last Name, Email, Password is required'
+        }
+        return response
+    hashed_string = hashlib.sha256(password.encode('utf-8')).hexdigest()
+    hashed = bcrypt.hashpw(hashed_string.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+    input_data = {
+        'first_name': first_name,
+        'last_name': last_name,
+        'email': email,
+        'password': hashed,
+        'user_guid': str(uuid.uuid4())
+    }
+    users_collection.insert_one(input_data)
+    response = {
+        'statusCode': 200,
+        'status': True,
+        'message': 'Successfully inserted',
+        'data': None
     }
     return response
 
 
+def logout(body: dict, authHeader: str) ->dict:
+    if not authHeader:
+        response = {
+            'statusCode': 401,
+            'status': False,
+            'message': 'Invalid ID or token',
+            'data': None
+        }
+        return response;
+    auth_token = authHeader.split(' ')
+    if(len(auth_token) == 1):
+        response = {
+            'statusCode': 401,
+            'status': False,
+            'message': 'Invalid ID or token',
+            'data': None
+        }
+        return response;
+    auth_token = auth_token[1]
+    user_guid = body.get('user_guid')
+    res = authTable.find_one({'user_guid':user_guid, 'auth_token':auth_token})
+    if not res:
+        response = {
+            'statusCode': 401,
+            'status': False,
+            'message': 'Invalid ID or token',
+            'data': None
+        }
+        return response;
+    res = authTable.delete_one({'user_guid':user_guid, 'auth_token':auth_token})
+    if res.deleted_count == 1:
+        response = {
+            'statusCode': 200,
+            'status': True,
+            'message': 'Logout Successfull',
+            'data': None
+        }
+        return response;
+    response = {
+        'statusCode': 500,
+        'status': False,
+        'message': 'Something went wrong',
+        'data': None
+    }
+    return response;
 
-{
-    "search_term":"taco",
-    "ser_guid":'abcd',
-}
